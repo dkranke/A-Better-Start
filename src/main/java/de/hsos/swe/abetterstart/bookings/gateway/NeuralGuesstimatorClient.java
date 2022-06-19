2f1322409d381e96cf8f147144a38158f5137579
Domenik Kranke <30910026+dkranke@users.noreply.github.com>
2022-06-19 16:41:24 +0200

    NeuralGuesstimatorClient: Replace GET with POST

--- src/main/java/de/hsos/swe/abetterstart/bookings/gateway/NeuralGuesstimatorClient.java
+++ src/main/java/de/hsos/swe/abetterstart/bookings/gateway/NeuralGuesstimatorClient.java
@@ -3,7 +3,7 @@ package de.hsos.swe.abetterstart.bookings.gateway;
 import de.hsos.swe.abetterstart.bookings.entity.BookingImportDTO;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 
 /**
  * As of the original class diagram, the NeuralGuesstimator is a REST client.
@@ -12,6 +12,6 @@ import javax.ws.rs.GET;
 @RegisterRestClient(baseUri = "http://localhost:8123/api")
 public interface NeuralGuesstimatorClient {
 
-    @GET
+    @POST
     long getExpectedDuration(BookingImportDTO booking);
 }
